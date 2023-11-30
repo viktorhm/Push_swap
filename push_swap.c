a2071d7b68160cb3243dc0c3f29e8eece282916d
Viktor Haratyk-Martin <vharatyk@c2r12p4.42lehavre.fr>
2023-11-30 10:45:08 +0100

    1%

---
+++ push_swap.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vharatyk <vharatyk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 18:37:17 by vharatyk          #+#    #+#             */
+/*   Updated: 2023/11/29 18:58:29 by vharatyk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+struct liste
+{
+
+} typedef ;
+
+
+//sa ntervertit les 2 premiers éléments au sommet de la pile a.Ne fait rien s’il n’y en a qu’un ou aucun.
+
+//sdIntervertit les 2 premiers éléments au sommet de la pile b.Ne fait rien s’il n’y en a qu’un ou aucun.
+
+//ss
+
+//pa
+
+//pb
+
+//ra
+
+//rb
+
+//rr
+//->ra, rb.
+
+//rra
+
+//rrb
+
+//rrr
